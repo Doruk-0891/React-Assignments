debdbe1fc7081971b83ac224d0a709e580f52064
mohdkashif <mohdkashif0891@gmail.com>
2023-09-20 10:22:55 +0530

    Assignment3 - Code

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
-
-const parent = React.createElement('div', {id: 'parent'},
-React.createElement('div', {id: 'child'},
-React.createElement('h1', {}, 'h1 tag -- heading tag')));
-
-console.log(parent);
+import { HeaderComponent } from './Header/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+root.render(<HeaderComponent />);
